2e3b67242ed5ed75a33f71685da03698c2fe4cda
Talha Anwar <37379131+talhaanwarch@users.noreply.github.com>
2020-11-09 21:37:55 +0500

    update

--- sub_app/py_templates/symptom_finder_ur.py
+++ sub_app/py_templates/symptom_finder_ur.py
@@ -12,17 +12,17 @@ def symp_finder(text,lang):
 		df.symp=df.symp.apply(lambda x:[stemmer.stemWord(i) for i in x])
 		text=text.lower().split()
 		text=[stemmer.stemWord(i) for i in text]
-		print(text)
+		print('english',text)
 	elif lang=='en-IN':#urdu
 		df.drop(['en_symp'],axis=1,inplace=True)
 		df.columns=['doctor','symp']
 		text=text.lower().split()
 		df.symp=df.symp.str.split(',')
+		print('urdu',text)
 	try:
 		
 		
-		print(df)
-		print(text)
+	
 		df.symp=df.symp.apply(lambda x: [i.strip() for i in x])
 		result=df.symp.apply(lambda x: set(x) & set(text)).dropna()
 		result=df['doctor'][result.map(lambda d: len(d)) > 0]
